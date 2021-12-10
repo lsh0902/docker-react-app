15bc16212f45a4379a7d648748d66d60d6b2d240
lsh0902 <hehesi2007@naver.com>
2021-12-11 02:56:51 +0900

    travis with aws

--- src/App.test.js
+++ src/App.test.js
@@ -2,5 +2,5 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  expect(1).toEqual(1);
+  expect(11).toEqual(11);
 });
